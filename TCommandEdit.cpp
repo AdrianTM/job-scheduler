55742a414c136e143957ca5e2c6681eafe74553e
Dolphin Oracle <dolphinoracle@gmail.com>
2021-02-04 18:50:55 -0500

    edit button label

--- TCommandEdit.cpp
+++ TCommandEdit.cpp
@@ -45,7 +45,7 @@ TCommandEdit::TCommandEdit(QWidget *parent)
             h->addWidget(new QLabel(tr("Time:")));
             h->addWidget((timeEdit = new QLineEdit()));
             h->addWidget((timeButton =
-                    new QPushButton(QIcon::fromTheme("edit-symbolic", QIcon(":/images/edit_small.png")), tr("E&dit"))));
+                    new QPushButton(QIcon::fromTheme("edit-symbolic", QIcon(":/images/edit_small.png")), tr("Time String E&ditor"))));
             timeButton->setMinimumSize(QSize(150, timeButton->maximumHeight()));
         }
         mainLayout->addSpacing(5);
